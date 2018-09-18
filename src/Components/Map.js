4f1ba23968c60e391be9f73430acc26e1d1add4f
Marium <mamsoille_2k@yahoo.com>
2018-09-18 13:46:09 -0700

    Added onclick marker functionality

--- src/Components/Map.js
+++ src/Components/Map.js
@@ -1,12 +1,17 @@
 import React, {Component} from 'react';
-import { withScriptjs, withGoogleMap, GoogleMap, Marker, MarkerClusterer } from'react-google-maps'
+import { withScriptjs, withGoogleMap, GoogleMap, Marker, InfoWindow } from'react-google-maps'
 
+//Displaying mu;ltiple markers https://stackoverflow.com/questions/43859785/how-do-i-display-multiple-markers-with-react-google-maps
 //https://medium.com/@yelstin.fernandes/render-a-map-component-using-react-google-maps-5f7fb3e418bb
 //pass markers array in return and then position of each marker
 class Map extends Component {
 
 constructor(props) {
     super(props)
+    this.state = {
+    selectedMarker:[],
+    position : null
+}
 }
 	/*getMarkers = () => {
 		for(let i=0; i<this.props.coffee_shops.length; i++) {
@@ -21,6 +26,14 @@ constructor(props) {
         }
 	}
 */
+	getInfoWindow = (marker) => {
+		
+		this.setState({selectedMarker:marker})
+		this.setState({position: {lat:marker.location.lat,lng:marker.location.lng}})
+		console.log(this.state.selectedMarker)
+		
+	}
+
 	render () {
 		const CoffeMap = withScriptjs(withGoogleMap((props) =>
 		<GoogleMap
@@ -30,18 +43,29 @@ constructor(props) {
 		
 		
 		{this.props.coffee_shops.map(marker => (
-        <Marker
+        <Marker 
+           key = {marker.id}
            position={{ lat: marker.location.lat, lng: marker.location.lng }}
            title = {marker.name}
+           onClick={e => {
+                this.getInfoWindow(marker);}}
+
         />
       )) }
+		{this.state.position && <InfoWindow position={this.state.position}>
+					     <h1>{this.state.selectedMarker.name}</h1>
+					 </InfoWindow>
+				 }
+
+		
+
 			
   		</GoogleMap>
   	))
 
 		return (
 			
-			<div>
+			<div >
 			<CoffeMap
 				isMarkerShown
   				googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyClnmi6Z3QxqK03n-h6vkgOp06Dc4jpOiw&v=3.exp&libraries=geometry,drawing,places"
